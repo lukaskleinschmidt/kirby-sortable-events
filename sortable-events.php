f9922b5189dfad5723885e1729d96a74aced35c2
lukaskleinschmidt <l.kleinschmidt@crolla-lowis.de>
2017-05-11 15:19:00 +0200

    Update

--- sortable-events.php
+++ sortable-events.php
@@ -1,11 +1,12 @@
 <?php
+
 // Make sure that the sortable plugin is loaded
 $kirby->plugin('sortable');
 
 if(!function_exists('sortable')) return;
 
-$kirby->set('field', 'events', __DIR__ . DS . 'field' . DS . 'events');
+$kirby->set('field', 'events', __DIR__ . DS . 'field');
 
 $sortable = sortable();
-$sortable->set('layout', 'event', __DIR__ . DS . 'layout' . DS . 'event');
-$sortable->set('variant', 'events', __DIR__ . DS . 'variant' . DS . 'events');
+$sortable->set('layout', 'event', __DIR__ . DS . 'layout');
+$sortable->set('variant', 'events', __DIR__ . DS . 'variant');
